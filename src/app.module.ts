9c4bea0a429e47478fa72e95d68cd5679b39da24
EllenBecker <ellenbeckerdc@gmail.com>
2023-03-22 16:37:01 -0300

    Config dotenv

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,16 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MailerModule } from '@nestjs-modules/mailer';
+import { ConfigModule } from '@nestjs/config';
+
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true
+    }),
     MailerModule.forRoot({
       transport: {
-        host: "sandbox.smtp.mailtrap.io",
-        port: 2525,
+        host: process.env.MAIL_HOST,
+        port: process.env.MAIL_PORT,
         auth: {
-          user: "813e27a8eb8d32",
-          pass: "5699d2e26b8ab8"
+          user: process.env.MAIL_USER,
+          pass: process.env.MAIL_PASSWORD
         }
       }
     })
